fff173c7df45404a6327080b3963a07e0fb0ca32
Torstein Sørnes <t.soernes@gmail.com>
2025-10-30 10:22:04 +0100

    agent2: disable list_history tool by default to rely on internal indices

--- crates/agent2/src/thread.rs
+++ crates/agent2/src/thread.rs
@@ -1,9 +1,9 @@
 use crate::{
     ChatHistoryAgentTool, ContextServerRegistry, CopyPathTool, CreateDirectoryTool,
     DbLanguageModel, DbThread, DeletePathTool, DiagnosticsTool, EditFileTool, EnhancedTerminalTool,
-    FetchTool, FindPathTool, GrepTool, ListDirectoryTool, ListHistoryTool, MemoryAgentTool,
-    MovePathTool, NowTool, OpenTool, ReadFileTool, ShellDetectorTool, SystemPromptTemplate,
-    Template, Templates, TerminalTool, ThinkingTool, TokenUsageTool, WebSearchTool,
+    FetchTool, FindPathTool, GrepTool, ListDirectoryTool, MemoryAgentTool, MovePathTool, NowTool,
+    OpenTool, ReadFileTool, ShellDetectorTool, SystemPromptTemplate, Template, Templates,
+    TerminalTool, ThinkingTool, TokenUsageTool, WebSearchTool,
 };
 use acp_thread::{MentionUri, UserMessageId};
 use action_log::ActionLog;
@@ -1795,7 +1795,7 @@ impl Thread {
         self.add_tool(FindPathTool::new(self.project.clone()));
         self.add_tool(GrepTool::new(self.project.clone()));
         self.add_tool(ListDirectoryTool::new(self.project.clone()));
-        self.add_tool(ListHistoryTool::new(cx.weak_entity()));
+        // self.add_tool(ListHistoryTool::new(cx.weak_entity())); // disabled by default
         self.add_tool(ChatHistoryAgentTool);
         self.add_tool(MemoryAgentTool::new(cx.weak_entity()));
         self.add_tool(TokenUsageTool::new(cx.weak_entity()));
